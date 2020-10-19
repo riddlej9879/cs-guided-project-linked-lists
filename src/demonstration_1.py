68f5193149b90fcd8d95ec944849a8b2d38dd866
riddlej9879 <riddlej9879@yahoo.com>
2020-10-19 17:41:23 -0500

    project done

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -1,42 +1,44 @@
 """
 Given only a reference to a specific node in a linked list, delete that node from a singly-linked list.
-
 Example:
-
-The code below should first construct a linked list (x -> y -> z) and then delete `y` from the linked list by calling the function `delete_node`. It is your job to write the `delete_node` function.
-
+The code below should first construct a linked list (x -> y -> z) and then delete `y` from the linked list by calling the function `delete_node`.
+It is your job to write the `delete_node` function.
 ```python
 class LinkedListNode():
     def __init__(self, value):
         self.value = value
         self.next  = None
-
 x = LinkedListNode('X')
 y = LinkedListNode('Y')
 z = LinkedListNode('Z')
-
 x.next = y
 y.next = z
-
 delete_node(y)
 ```
-
 *Note: We can do this in O(1) time and space! But be aware that our solution will have some side effects...*
 """
-class LinkedListNode():
+class LinkedListNode:
     def __init__(self, value):
         self.value = value
         self.next  = None
-
 def delete_node(node_to_delete):
     # Your code here
-
-
+    # Set node_to_delete.value to node_to_delete.next.value
+    node_to_delete.value = node_to_delete.next.value
+    # Set node_to_delete.next to  node_to_delete.next.next
+    node_to_delete.next = node_to_delete.next.next
 x = LinkedListNode('X')
 y = LinkedListNode('Y')
 z = LinkedListNode('Z')
-
 x.next = y
 y.next = z
-
+cur_node = x
+while cur_node:
+    print(cur_node.value)
+    cur_node = cur_node.next
+print("------")
 delete_node(y)
+cur_node = x
+while cur_node:
+    print(cur_node.value)
+    cur_node = cur_node.next
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -2,13 +2,10 @@
 Given a reference to the head node of a singly-linked list, write a function
 that reverses the linked list in place. The function should return the new head
 of the reversed list.
-
 In order to do this in O(1) space (in-place), you cannot make a new list, you
 need to use the existing nodes.
-
 In order to do this in O(n) time, you should only have to traverse the list
 once.
-
 *Note: If you get stuck, try drawing a picture of a small linked list and
 running your function by hand. Does it actually work? Also, don't forget to
 consider edge cases (like a list with only 1 or 0 elements).*
@@ -16,24 +13,48 @@ consider edge cases (like a list with only 1 or 0 elements).*
 class LinkedListNode():
     def __init__(self, value):
         self.value = value
-        self.next  = None
-
+        self.next = None
 def reverse(head_of_list):
+    # Reverse all the pointers:
+    # Loop (while) go while… we still have nodes to process
+    # We have a current_node
+    # Initialize current_node
     current_node = head_of_list
-    previous_node = None
-    next_node = None
-
-    # Until we have 'fallen off' the end of the list
-    while current_node:
-        # Copy a pointer to the next element
-        # before we overwrite current_node.next
+    # We need a reference to prev
+    # When we start, prev is none
+    prev = None
+    # On each iteration of the loop:
+    while current_node is not None:
+        print("Current node:", current_node.value)
+    # We need a reference to next_node:
+    # Next_node = current_node.next
         next_node = current_node.next
-
-        # Reverse the 'next' pointer
-        current_node.next = previous_node
-
-        # Step forward in the list
-        previous_node = current_node
+    # Set current_node.next to prev
+        current_node.next = prev
+    # Set prev to cur_node
+        prev = current_node
+    # Update current_node to point to next_node
         current_node = next_node
-
-    return previous_node
+    return current_node
+x = LinkedListNode('X')
+y = LinkedListNode('Y')
+z = LinkedListNode('Z')
+x.next = y
+y.next = z
+cur_node = x
+while cur_node:
+    print(cur_node.value)
+    cur_node = cur_node.next
+print("------")
+new_head = reverse(x)
+print(new_head)
+#
+# print("reversed list:")
+# cur_node = new_head
+# while cur_node:
+#     print(cur_node.value)
+#     cur_node = cur_node.next
+# print("-----")
+# print(x.value, x.next)
+# print(y.value, y.next.value)
+# print(z.value, z.next)
